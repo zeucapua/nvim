dbef615dae92d83946cf365bb5ba24cbae14f6e4
Zeu Capua <coding@zeu.dev>
2025-02-18 08:50:57 -0800

    show relative path on lualine, set keymaps for diagnostic goto

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,3 +1,12 @@
 require("lualine").setup({
-  options = { theme = "horizon" }
+  options = { theme = "horizon" },
+  sections = {
+    lualine_b = {'branch'},
+    lualine_c = {{
+      'filename',
+      path = 1
+    }},
+    lualine_x = {},
+    lualine_y = {'diff', 'diagnostics'}
+  }
 })
--- lua/zeu/remap.lua
+++ lua/zeu/remap.lua
@@ -3,3 +3,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pq", vim.cmd.q)
 vim.keymap.set("n", "<leader>pl", vim.cmd.split)
 vim.keymap.set("n", "<leader>pv", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>[[", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>]]", vim.diagnostic.goto_next)
