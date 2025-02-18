22fe7b2ad809990a9110c2801e42caf62e7b19b6
Zeu Capua <coding@zeu.dev>
2025-02-18 11:40:45 -0800

    show relative path in lualine for inactive panes

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -4,9 +4,15 @@ require("lualine").setup({
     lualine_b = {'branch'},
     lualine_c = {{
       'filename',
-      path = 1
+      path = 1 -- show relative path 
     }},
     lualine_x = {},
     lualine_y = {'diff', 'diagnostics'}
+  },
+  inactive_sections = {
+    lualine_c = {{
+      'filename',
+      path = 1 -- show relative path
+    }}
   }
 })
