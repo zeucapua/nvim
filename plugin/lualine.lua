c1989cd1d62a9b9b59cce8cba2dae2dc7d3781de
zeudev <coding@zeu.dev>
2025-08-30 15:45:30 -0700

    update lualine

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -1,7 +1,16 @@
 require("lualine").setup({
   options = { theme = "horizon" },
   sections = {
-    lualine_b = {'branch'},
+    lualine_b = {{
+      'branch',
+      fmt = function(str)
+        if vim.api.nvim_strwidth(str) > 13 then
+            return ("%s…"):format(str:sub(1, 12))
+        end
+
+        return str
+      end
+    }},
     lualine_c = {{
       'filename',
       path = 1 -- show relative path 
