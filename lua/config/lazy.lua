bb6acf19ed748e2209606d2c072b38d841659a9f
zeu <zeu@zeus-MacBook-Pro.local>
2025-09-11 01:47:19 -0700

    add telescope and nvim tree

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -59,10 +59,32 @@ require("lazy").setup({
     { "xiyaowong/nvim-transparent" },
     { "lukas-reineke/indent-blankline.nvim" },
     { "nvim-tree/nvim-tree.lua" },
-    { "theprimeagen/harpoon" },
+    {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" }
+    },
     {
       'nvim-lualine/lualine.nvim',
       dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+
+    {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      lazy = false,
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end,
     },
 
     { 
